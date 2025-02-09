9d1bc9542a56dbc2b3e129c0790369068a79d8ff
Juan David Castro Gallego <fjuandc@gmail.com>
2025-02-09 00:37:54 -0500

    refactor: cleaner items service base items favorite prop implementation

--- src/api/items/ExternalApiRequest/ExternalApiRequestItems.service.ts
+++ src/api/items/ExternalApiRequest/ExternalApiRequestItems.service.ts
@@ -10,7 +10,7 @@ export class ExternalApiRequestItemsService extends ItemsEntity {
     return categories;
   }
 
-  private _transformBaseItem(originalItem: any, isFavorite: boolean): BaseItem {
+  private _transformBaseItem(originalItem: any): BaseItem {
     const item: BaseItem = {
       id: originalItem.id,
       title: originalItem.title,
@@ -22,47 +22,55 @@ export class ExternalApiRequestItemsService extends ItemsEntity {
       picture: originalItem.thumbnail,
       condition: originalItem.condition,
       free_shipping: originalItem.shipping.free_shipping,
-      favorite: isFavorite,
+      favorite: originalItem.favorite,
     };
     return item;
   }
 
-  private _transformDetailedItem(originalItem: any, dataDesc: any, isFavorite: boolean): DetailedItem {
-    const baseItem = this._transformBaseItem(originalItem, isFavorite);
+  private _transformDetailedItem(originalItem: any, dataDesc: any): DetailedItem {
+    const baseItem = this._transformBaseItem(originalItem);
     const detailedItem: DetailedItem = {
       ...baseItem,
       sold_quantity: originalItem.initial_quantity, // TODO: ??
       description: dataDesc.plain_text,
-      favorite: isFavorite,
     };
     return detailedItem;
   }
 
   private _transformSearchItems(originalResults: any) {
-    const items: BaseItem[] = originalResults.map((item: any) => this._transformBaseItem(item, item.favorite));
+    const items: BaseItem[] = originalResults.map(this._transformBaseItem);
     return items;
+  }
+
+  private _addFavoriteToItems(originalItems: any) {
+    const ids = originalItems.map((item: any) => item.id);
+    const favoriteIds = this._favoritesService.validateByIds(ids);
+    return originalItems.map((item: any) => ({ ...item, favorite: favoriteIds[item.id] }));
+  }
+
+  private _addFavoriteToItem(originalItem: any) {
+    let isFavorite = false;
+    try {
+      isFavorite = !!this._favoritesService.getById(originalItem.id);
+    } catch {
+      isFavorite = false;
+    }
+    return { ...originalItem, favorite: isFavorite };
   }
 
   public async getBySearch(query: string) {
     const original = await ExternalApiRequestItemsData.originalSearchResults(query);
-    const ids = original.results.map((item: any) => item.id);
-    const favoriteIds = this._favoritesService.validateByIds(ids);
-    const originalWithFavorite = original.results.map((item: any) => ({ ...item, favorite: favoriteIds[item.id] }));
+    const originalWithFavorites = this._addFavoriteToItems(original.results);
     const categories = this._transformCategories(original.categories.path_from_root);
-    const items = this._transformSearchItems(originalWithFavorite);
+    const items = this._transformSearchItems(originalWithFavorites);
     return { categories, items };
   }
 
   public async getById(id: string) {
     const { data, dataDesc, dataCategory } = await ExternalApiRequestItemsData.originalItem(id);
-    let isFavorite = false;
-    try {
-      isFavorite = !!this._favoritesService.getById(id);
-    } catch {
-      isFavorite = false;
-    }
+    const dataWithFavorite = this._addFavoriteToItem(data);
     const categories = this._transformCategories(dataCategory.path_from_root);
-    const item = this._transformDetailedItem(data, dataDesc, isFavorite);
+    const item = this._transformDetailedItem(dataWithFavorite, dataDesc);
     return { categories, item };
   }
 }
